cb8991ceb21651b88258aa9241a26aa5c570c93c
Chetan Wadate <chetanwadate@gmail.com>
2016-02-06 03:12:46 +0530

    Struts2

---
+++ src/com/compsgeek/struts/action/TutorialAction.java
@@ -0,0 +1,10 @@
+package com.compsgeek.struts.action;
+
+public class TutorialAction {
+
+	public String execute(){
+		System.out.println("hello from execute method");
+		return "failure";
+	}
+	
+}
